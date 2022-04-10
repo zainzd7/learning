cf3d4fa6f14ad43896db3c37d7617e85990be8d1
Yousuf Ashparie <yousuf.ashparie@expel.io>
2022-04-10 13:46:37 +0200

    arrays exercise 1

---
+++ arrays.py
@@ -0,0 +1,20 @@
+import random
+# given a list of numbers
+numbers = [5, 4, 8, 23, 7, 9, 2, 14, 18, 10]
+
+# add the random number to the list
+random_number = random.randint(0, 100)
+
+print(numbers)
+
+# find the largest number
+largest_number = 0
+print(f'The largest number is: {largest_number}')
+
+# find the smallest number
+smallest_number = 0
+print(f'The smallest number is: {largest_number}')
+
+
+# create 2 functions based on the above
+
