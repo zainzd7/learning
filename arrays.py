8b9216efa43e7cc5d207c9d1d4d8da8dc7f5b993
Zain <zain@MacBook-Pro.fritz.box>
2022-04-24 18:02:32 +0200

    edited arrays

--- arrays.py
+++ arrays.py
@@ -1,28 +1,19 @@
-import random
-# given a list of numbers
-numbers = [5, 4, 8, 23, 25, 56, 7, 9, 2, 14, 18, 10]
-
-# add the random number to the list, hint numbers.append
-random_number = random.randint(0, 100)
-numbers.append(random_number)
-print(numbers)
 
 # find the largest number
-largest_number = 0
-for number in numbers:
-    if number > largest_number:
-        largest_number = number
+def find_largest_number(numbers):
+    largest_number = 0
+    for number in numbers:
+        if number > largest_number:
+            largest_number = number
 
-print(f'The largest number is: {largest_number}')
+    return largest_number
+
 
 # find the smallest number
-smallest_number = numbers[0]
-for number in numbers:
-    if number < smallest_number:
-        smallest_number = number
+def find_smallest_number(numberz):
+    smallest_number = numberz[0]
+    for number in numberz:
+        if number < smallest_number:
+            smallest_number = number
 
-print(f'The smallest non-zero number is: {smallest_number}')
-
-
-# create 2 functions based on the above
-
+    print(f'The smallest non-zero number is: {smallest_number}')
--- hello.py
+++ hello.py
@@ -1,1 +1,4 @@
-print("hello")
+from arrays import find_largest_number
+
+print(find_largest_number([1, 2, 5, 67, 98, 78, 56]))
+
